be6174f89391eca603bfd712188f801a3a004995
Danny <dyaroslavski@gmail.com>
2014-04-02 12:06:29 -0400

    Setup for collision

--- mobile2d/src/Test2D.hx
+++ mobile2d/src/Test2D.hx
@@ -44,6 +44,68 @@ class Poly
     }
 }
 
+class Collision
+{
+    /**
+     * Return a 1D segment with the 2D projection values of 2D Polygon projected on a
+     * 2D Vector vec, ordered (min,max)*/
+    public static function projectPolyOnVec(poly:Poly,vec:Point):Point{
+        return new Point(1,1);
+    }
+
+    /**
+     * Return a 1D segment with the 2D projection values of 2D Circle projected on a
+     * 2D Vector vec, ordered (min,max)*/
+    public static function projectCircleOnVec(pos:Point, radius:Float, vec:Point):Point{
+        return new Point(0,0);
+    }
+
+    /**
+     * Given two 1D segments, return overlap distance from v0 to v1 if 
+     * exists 1D overlap, or -1 if there is no overlap
+     */
+    public static function getOverlap(v0:Point, v1:Point):Float{
+        //assuming v0(min,max) and v1(min,max)
+        var start:Float = Math.min(v0.x,v1.x);
+        var end:Float = Math.max(v0.y,v1.y);
+        var dist = end-start;
+        return (dist<0)?-1:dist;
+    }
+
+    public static function getNormals(poly:Poly):Vector<Point>{
+        var vec:Vector<Point> = new Vector<Point>();
+        vec.push(new Point(1,0));
+        return vec;
+    }
+
+    /**
+     * Given a circle and convex polygon, return shortest overlap vector if there is
+     * a collision or null if there is no collision
+     */
+    public static function getOverlapCirclePoly(circlePos:Point, radius:Float, poly:Poly):Point{
+        var normals:Vector<Point> = getNormals(poly);
+        var i:Int =0;
+        var shortestOverlap:Float=0;
+        var shortestVec:Point = null;
+        for(normal in normals){
+            var circleProj:Point = projectCircleOnVec(circlePos,radius,normal);
+            var polyProj:Point = projectPolyOnVec(poly,normal);
+            var overlap:Float = getOverlap(circleProj,polyProj);
+            if(overlap==-1){
+                return null;
+            }
+            if(shortestVec==null||overlap<shortestOverlap){
+                shortestOverlap=overlap;
+                //flip vector if projections are in the other dir
+                if(polyProj.x<circleProj.x){
+                    overlap*=-1;
+                }
+            }
+        }
+        return shortestVec;
+    }
+}
+
 class Test2D extends Sprite
 {		
     //engine variables
@@ -101,6 +163,18 @@ class Test2D extends Sprite
             char.y+=yd;
         }
     }
+    private function checkCollisions():Void {
+        var overlapVec:Point;
+        var charPos:Point = new Point(char.x,char.y);
+        for(poly in polygons){
+            overlapVec =Collision.getOverlapCirclePoly(charPos,char.radius,poly);
+            if(overlapVec!=null){
+                charPos = new Point(charPos.x+overlapVec.x,charPos.y+overlapVec.y);
+                char.x=charPos.x;
+                char.y=charPos.y;
+            }
+        }
+    }
 
     private function renderChar():Void {
         graphics.clear();
@@ -120,9 +194,11 @@ class Test2D extends Sprite
         }
     }
 
+
     private function enterframe(e:Event):Void
     {
         moveChar();
+        checkCollisions();
         renderChar();
         renderPolys();
     }
